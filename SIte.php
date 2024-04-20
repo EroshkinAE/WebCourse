9795fcce64fdf4b9c90b87e2bb648cbf58539c5d
EroshkinAE <komp1711@gmail.com>
2024-04-20 22:23:24 +0700

    new site

--- SIte.php
+++ SIte.php
@@ -2,11 +2,15 @@
 <html lang="en">
 
 <head>
-    <title>Document</title>
+    <title>SITE</title>
 </head>
 
 <body>
-    <p>SUBADUBA</p>
-    <p>papa lox</p>
+    <p>ЭТО ВТОРОЙ</p>
+    <p>Vot moia silka <a href="https://www.youtube.com/watch?v=akCPm_iGyR4" title="vidos na YouTube" target="_blank"> govna </a>.</p>
+    <p>Ссылка на мой <a href="https://www.mozilla.org/" title="Домашняя страница Mozilla" target="_blank">любимый веб-сайт</a>.</p>
+    <p><input type="text" disabled /></p>
+    <p><input type="text" /></p>
 </body>
+
 </html>
